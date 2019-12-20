84fdb74af7bbd22809e588bee3774ab0d230ba2f
Jan Julius <jdelang@hotmail.com>
2019-12-20 13:31:51 +0100

    add: Skill base class

--- Cold rain unity/Assets/Scripts/skills/Skills.cs
+++ Cold rain unity/Assets/Scripts/skills/Skills.cs
@@ -9,6 +9,7 @@ public class Skills : Node, SavingModule
     public static readonly string[] Skill = { "Warrior", "Archer", "Hunting", "Fishing", "Farming", "Cooking", "Artisan" };
 
     public static readonly int WARRIOR = 0, ARCHER = 1, HUNTING = 2, FISHING = 3, FARMING = 4, COOKING = 5, ARTISAN = 6;
+    
 
     public void Load()
     {
