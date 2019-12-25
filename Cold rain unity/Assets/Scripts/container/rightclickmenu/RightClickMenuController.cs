dc27d4d99cc7dceb8c5943454287a939a5ceca4f
thosmas <thomas.tijsma@student.nhlstenden.com>
2019-12-25 15:05:38 +0100

    Start to rightclickmenu

---
+++ Cold rain unity/Assets/Scripts/container/rightclickmenu/RightClickMenuController.cs
@@ -0,0 +1,59 @@
+﻿using Assets.Scripts.container;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class RightClickMenuController : MonoBehaviour, IPointerClickHandler
+{
+    public Button sampleButton;
+    private List<RightClickMenuItem> contextMenuItems;
+    private ItemSlot parentObject;
+    //private MainMenuManager mm;
+    //private ChatManager cm;
+
+    void Awake()
+    {
+        contextMenuItems = new List<RightClickMenuItem>();
+        parentObject = GetComponent<ItemSlot>();
+        //mm = FindObjectOfType<MainMenuManager>();
+
+        Action<Image> use = new Action<Image>(UseAction);
+        Action<Image> examine = new Action<Image>(ExamineAction);
+        Action<Image> drop = new Action<Image>(DropAction);
+        
+        contextMenuItems.Add(new RightClickMenuItem("Use", sampleButton, use));
+        contextMenuItems.Add(new RightClickMenuItem("Examine", sampleButton, examine));
+        contextMenuItems.Add(new RightClickMenuItem("Drop", sampleButton, drop));
+
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (eventData.button == PointerEventData.InputButton.Right)
+        {
+            //mm.AtopSocialLeftBar();
+            print("Right click on rightclickmenucontroller");
+            RightClickMenu.Instance.CreateContextMenu(contextMenuItems,
+                new Vector2(this.GetComponent<RectTransform>().offsetMin.x - 40,
+                this.GetComponent<RectTransform>().offsetMin.y));
+        }
+    }
+
+    private void ExamineAction(Image obj)
+    {
+        throw new NotImplementedException();
+    }
+
+    private void DropAction(Image obj)
+    {
+        throw new NotImplementedException();
+    }
+
+    private void UseAction(Image obj)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Cold rain unity/Assets/Scripts/container/rightclickmenu/RightClickMenuItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RightClickMenuItem : MonoBehaviour
+{
+    public string text;
+    public Button button;
+    public Action<Image> action;
+
+    public RightClickMenuItem(string text, Button button, Action<Image> action)
+    {
+        this.text = text;
+        this.button = button;
+        this.action = action;
+    }
+}
