24c16f04d9a4bdbdb9a4874fece870ce9c62ed9d
thosmas <thomas.tijsma@student.nhlstenden.com>
2020-01-05 12:46:36 +0100

    startscreen is now a startscreen
    still has some issues to fix

---
+++ Cold rain unity/Assets/Scripts/menu/mainMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class mainMenu : MonoBehaviour
+{
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+}
