3dc5fc185312e5b320cb4dacbfb9d946a0398e8c
thosmas <thomas.tijsma@student.nhlstenden.com>
2020-01-05 14:26:27 +0100

    Rain pogchamp

---
+++ Cold rain unity/Assets/Scripts/menu/TextureOffsetter.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextureOffsetter : MonoBehaviour
+{
+
+    public float scrollSpeedX = 0.5f;
+    public float scrollSpeedY = 0;
+    public string name = "_MainTex";
+    private Material r;
+
+    void Start()
+    {
+        if (GetComponent<SkinnedMeshRenderer>() != null)
+        {
+            r = GetComponent<SkinnedMeshRenderer>().materials[0];
+        }
+        else if (GetComponent<MeshRenderer>() != null)
+        {
+            r = GetComponent<MeshRenderer>().materials[0];
+        }
+        else
+        {
+            Debug.LogError("There is no kind of mesh renedere attatched");
+        }
+    }
+
+    void Update()
+    {
+        r.SetTextureOffset(name, new Vector2(Time.time * scrollSpeedX, Time.time * scrollSpeedY));
+    }
+}
