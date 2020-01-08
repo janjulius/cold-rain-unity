1bccf5347373b9acde37bc06aaa020cf9a8c651f
thosmas <thomas.tijsma@student.nhlstenden.com>
2020-01-08 16:54:55 +0100

    Added shops to the NPCs

--- Cold rain unity/Assets/Scripts/shops/constants/ShopConstants.cs
+++ Cold rain unity/Assets/Scripts/shops/constants/ShopConstants.cs
@@ -9,5 +9,9 @@ namespace Assets.Scripts.shops.constants
     public class ShopConstants
     {
         public static int TEST_SHOP = 0;
+        public static int HUNTING_SHOP = 1;
+        public static int FISHING_SHOP = 2;
+        public static int CROP_SHOP = 3;
+        public static int LIVESTOCK_SHOP = 4;
     }
 }
