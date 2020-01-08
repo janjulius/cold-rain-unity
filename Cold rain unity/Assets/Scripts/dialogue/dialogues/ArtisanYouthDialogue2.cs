d7b4cfb77935ca03dad64181e22273bbc3751017
thosmas <thomas.tijsma@student.nhlstenden.com>
2020-01-08 14:44:38 +0100

    More NPCs

---
+++ Cold rain unity/Assets/Scripts/dialogue/dialogues/ArtisanYouthDialogue2.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts.dialogue.dialogues
+{
+    class ArtisanYouthDialogue2 : Dialogue
+    {
+        public override void Initiate()
+        {
+            base.Initiate();
+        }
+
+        public override void Handle()
+        {
+            base.Handle();
+            switch (stage)
+            {
+                case 0:
+                    Player("Get to work");
+                    stage++;
+                    break;
+                case 1:
+                    Npc("Okay boss");
+                    stage = 100;
+                    break;
+                case 100:
+                    End();
+                    break;
+            }
+        }
+
+        public override void End(object[] args)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool Open(object[] args)
+        {
+            Npc($"'Sup boss");
+            stage = 0;
+            return true;
+        }
+    }
+}
