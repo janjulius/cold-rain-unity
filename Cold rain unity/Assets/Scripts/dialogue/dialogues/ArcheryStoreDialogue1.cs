8f849aa8c5f13c60f28cc1c8b1e17c33dad3f20f
thosmas <thomas.tijsma@student.nhlstenden.com>
2020-01-10 14:12:07 +0100

    Added all store counters

---
+++ Cold rain unity/Assets/Scripts/dialogue/dialogues/ArcheryStoreDialogue1.cs
@@ -0,0 +1,42 @@
+﻿using Assets.Scripts.shops.constants;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts.dialogue.dialogues
+{
+    class ArcheryStoreDialogue1 : Dialogue
+    {
+        public override void Initiate()
+        {
+            base.Initiate();
+        }
+
+        public override void Handle()
+        {
+            base.Handle();
+            switch (stage)
+            {
+                case 0:
+                    OpenShop(ShopConstants.ARCHER_SHOP_WEAPONRY);
+                    End();
+                    break;
+            }
+        }
+
+        public override void End(object[] args)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool Open(object[] args)
+        {
+            base.Open(args);
+            Npc($"Would you Like to browse our selection?");
+            stage = 0;
+            return true;
+        }
+    }
+}
