82227ce720794d9504db752b4feb96ceca73cf6e
Thefellowes <marcogeertsma@gmail.com>
2020-01-13 16:22:08 +0100

    Add: Start worldstate manager and more clothes

---
+++ Cold rain unity/Assets/Scripts/managers/WorldStateManager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts.managers
+{
+    class WorldStateManager : Node
+    {
+        public Dictionary<int, short> worldStateDict = new Dictionary<int, short>();
+        private short currentWorldState = 0;
+
+
+        public override void Initiate()
+        {
+
+        }
+
+        public int this[int id]
+        {
+            get
+            {
+                if (!worldStateDict.ContainsKey(id))
+                {
+                    worldStateDict.Add(id, currentWorldState);
+                }
+                return worldStateDict[id];
+            }
+        }
+
+        public void setWorldState(int id, short worldState)
+        {
+            
+        }
+    }
+}
