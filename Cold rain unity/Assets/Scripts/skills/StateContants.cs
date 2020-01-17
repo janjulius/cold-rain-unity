abc3c3a741df4322c239e86d2c08ed5198b34b16
thosmas <thomas.tijsma@student.nhlstenden.com>
2020-01-17 15:09:57 +0100

    Artisan states

---
+++ Cold rain unity/Assets/Scripts/skills/StateContants.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts.shops.constants
+{
+    public class StateConstants
+    {
+        public static int ARTISAN_FIREPLACE_1 = 0;
+        public static int KITCHEN = 1;
+        public static int ARTISAN_INTERFACEINTERACT = 2;
+        public static int ARTISAN_MACHINE = 3;
+        public static int ARTISAN_FIREPLACE_2 = 4;
+        public static int ARTISAN_JAM_IDLE = 5;
+        public static int ARTISAN_JAM_DONE = 6;
+        public static int ARTISAN_MILK_IDLE = 7;
+        public static int ARTISAN_MILK_DONE = 8;
+        public static int ARTISAN_CHEESE_IDLE = 9;
+        public static int ARTISAN_CHEESE_DONE = 10;
+    }
+}
