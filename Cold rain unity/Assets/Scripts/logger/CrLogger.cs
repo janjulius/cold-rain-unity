2a566f3c45878159ae2ff8e53caabffea9a24ba5
Jan Julius <jdelang@hotmail.com>
2020-01-26 15:18:38 +0100

    updated: logger

--- Cold rain unity/Assets/Scripts/logger/CrLogger.cs
+++ Cold rain unity/Assets/Scripts/logger/CrLogger.cs
@@ -4,12 +4,15 @@ using UnityEngine;
 
 namespace Assets.Scripts.logger
 {
-    public class CrLogger
+    public class CrLogger : ILogger
     {
         public static List<string> logs = new List<string>();
 
-        public static void Log(object caller, string message)
-            => Log(caller, message, LogType.DEFAULT);
+        public ILogHandler logHandler { get; set; }
+        public bool logEnabled { get; set; }
+        public UnityEngine.LogType filterLogType { get; set; }
+
+        public static void Log(object caller, string message) => Log(caller, message, LogType.DEFAULT);
 
         public static void Log(object caller, string message, LogType type)
         {
@@ -17,32 +20,61 @@ namespace Assets.Scripts.logger
             switch (type)
             {
                 case LogType.WARNING:
-                    //Debug.LogWarning(msg);
-                    logs.Add($"{DateTime.Now.ToString("h: mm:ss")} [WARNING] {msg}");
+                    logs.Add($"{GetTimeAndDate()} {msg}");
                     break;
                 case LogType.ERROR:
-                    //Debug.LogError(msg);
-                    logs.Add($"{DateTime.Now.ToString("h: mm:ss tt")} [ERROR] {msg}");
+                    logs.Add($"{GetTimeAndDate()} {msg}");
                     break;
                 case LogType.DEVELOPER:
-                    //Debug.Log(msg);
-                    logs.Add($"{DateTime.Now.ToString("h: mm:ss tt")} [DEVELOPER] {msg}");
+                    logs.Add($"{GetTimeAndDate()} {msg}");
                     break;
                 case LogType.FILTERED:
-                    logs.Add($"{DateTime.Now.ToString("h: mm:ss tt")} [FILTERED] {msg}");
+                    logs.Add($"{GetTimeAndDate()} {msg}");
                     break;
                 case LogType.CONSOLE:
-                    logs.Add($"{DateTime.Now.ToString("h: mm:ss tt")} [CONSOLE] {msg}");
+                    logs.Add($"{GetTimeAndDate()} {msg}");
                     break;
                 default:
-                    //Debug.Log(msg);
-                    logs.Add($"{DateTime.Now.ToString("h: mm:ss tt")} [DEFAULT] {msg}");
+                    logs.Add($"{GetTimeAndDate()} {msg}");
                     break;
             }
         }
 
         private static string ConstructMessage(object caller, string message, LogType type)
-            => $"[{caller}] {message}";
+            => $"[{type.ToString()}] [{caller}] {message}";
+
+        private static string ConstructMessage(object caller, string message, LogType type, UnityEngine.Object context)
+             => $"[{type.ToString()}] [{caller}] {message} CTX: {context.name}";
+
+        private static string ConstructMessage(object caller, string message, UnityEngine.LogType logtype)
+            => $"[{logtype.ToString()}] [{caller}] {message}";
+
+        private static string ConstructMessage(object caller, string message, UnityEngine.LogType logtype, UnityEngine.Object context)
+            => $"[{logtype.ToString()}] [{caller}] {message} CTX: {context.name}";
+
+        public void LogError(string tag, object message) => logs.Add($"{GetTimeAndDate()} {ConstructMessage(tag, message.ToString(), LogType.ERROR)}");
+        public void LogError(string tag, object message, UnityEngine.Object context) => logs.Add($"{GetTimeAndDate()} {ConstructMessage(tag, message.ToString(), LogType.ERROR)} CTX: {context.ToString()}");
+
+        public void LogException(Exception exception) => logs.Add($"{GetTimeAndDate()} {ConstructMessage(exception.ToString(), exception.StackTrace.ToString(), LogType.EXCEPTION)}");
+        public void LogException(Exception exception, UnityEngine.Object context) => logs.Add($"{GetTimeAndDate()} {ConstructMessage(exception.ToString(), exception.StackTrace.ToString(), LogType.EXCEPTION)} CTX: {context.name}");
+
+        public void LogWarning(string tag, object message) => logs.Add($"{GetTimeAndDate()} {ConstructMessage(tag, message.ToString(), LogType.WARNING)}");
+        public void LogWarning(string tag, object message, UnityEngine.Object context) => logs.Add($"{GetTimeAndDate()} {ConstructMessage(tag, message.ToString(), LogType.WARNING)} CTX: {context.name}");
+
+        public void Log(UnityEngine.LogType logType, object message) => logs.Add(ConstructMessage(null, message.ToString(), logType));
+        public void Log(UnityEngine.LogType logType, object message, UnityEngine.Object context) => logs.Add(ConstructMessage(context.name, message.ToString(), logType, context));
+        public void Log(UnityEngine.LogType logType, string tag, object message) => logs.Add(ConstructMessage(tag, message.ToString(), logType));
+        public void Log(UnityEngine.LogType logType, string tag, object message, UnityEngine.Object context) => logs.Add(ConstructMessage(tag, message.ToString(), logType, context));
+
+        public void Log(object message) => logs.Add(ConstructMessage(null, message.ToString(), LogType.LOG));
+        public void Log(string tag, object message) => logs.Add(ConstructMessage(tag, message.ToString(), LogType.LOG));
+        public void Log(string tag, object message, UnityEngine.Object context) => logs.Add(ConstructMessage(tag, message.ToString(), LogType.LOG, context));
+
+        public void LogFormat(UnityEngine.LogType logType, string format, params object[] args) { }
+        public void LogFormat(UnityEngine.LogType logType, UnityEngine.Object context, string format, params object[] args) { }
+
+        private static string GetTimeAndDate() => DateTime.Now.ToString("hh:mm:ss");
+        public bool IsLogTypeAllowed(UnityEngine.LogType logType) => true;
 
         public enum LogType
         {
@@ -51,7 +83,9 @@ namespace Assets.Scripts.logger
             ERROR,
             DEVELOPER,
             FILTERED,
-            CONSOLE
+            CONSOLE,
+            EXCEPTION,
+            LOG
         }
     }
 }
